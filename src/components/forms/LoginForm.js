537b294f686c4ada28bd81d2522e0d3d0336c7e8
BoddetiAlekhya <boddeti.alekhya@gmail.com>
2019-02-25 18:54:18 +0530

    Server Connection

--- src/components/forms/LoginForm.js
+++ src/components/forms/LoginForm.js
@@ -1,6 +1,6 @@
 import React,{Component} from 'react';
 import PropTypes from 'prop-types'
-import {Form, Button} from 'semantic-ui-react'
+import {Form, Button,Message} from 'semantic-ui-react'
 import Validator from 'validator'
 import InlineError from '../messages/InlineError'
 
@@ -18,12 +18,16 @@ class LoginForm extends Component {
     onChange = e => this.setState({data: {...this.state.data,[e.target.name]: e.target.value}})
 
     onSubmit = () => {
+        
         const errors = this.validate(this.state.data);
         this.setState({errors});
-        if(Object.keys(errors).length === 0){
-            this.props.submit(this.state.data);
+        if(Object.keys(errors).length === 0) {
+            this.setState({loading: true})
+            this.props
+            .submit(this.state.data)
+            .catch(err => this.setState({errors: err.response.data.errors,loading: false}));
         }
-    }
+    };
 
     validate = (data) => {
         const errors = {};
@@ -33,18 +37,34 @@ class LoginForm extends Component {
     }
 
     render () {
-        const { data,errors } = this.state;
+        const { data,errors,loading } = this.state;
         return (
-            <Form onSubmit = {this.onSubmit}>
+            <Form onSubmit = {this.onSubmit} loading={loading}>
+                {errors.global && (
+                <Message negative>
+                    <Message.Header>Something Went Wrong </Message.Header>
+                    <p>{errors.global}</p>
+                </Message>
+                )}
                 <Form.Field error={!!errors.email}>
                     <label htmlFor="email">Email</label>
-                    <input type="email" id="email" name="email" placeholder="example@example.com" value={data.email} onChange={this.onChange} />
+                    <input 
+                    type="email" 
+                    id="email" 
+                    name="email" placeholder="example@example.com" 
+                    value={data.email} 
+                    onChange={this.onChange} />
 
                     {errors.email && <InlineError text={errors.email} />}
                 </Form.Field>
                 <Form.Field error={!!errors.password}>
                     <label htmlFor="password">Password</label>
-                    <input type="password" id="password" name="password" placeholder="Make it Secure" value={data.password} onChange={this.onChange}/>
+                    <input type="password"
+                     id="password"
+                     name="password" 
+                     placeholder="Make it Secure" 
+                     value={data.password} 
+                     onChange={this.onChange}/>
 
                     {errors.password && <InlineError text={errors.password} />}
                 </Form.Field>
