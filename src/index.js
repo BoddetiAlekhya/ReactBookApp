af47dd83cce6b69057a3c2988c5dd93c010097e5
BoddetiAlekhya <boddeti.alekhya@gmail.com>
2019-02-25 12:50:57 +0530

    semantic ui setup

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {BrowserRouter} from 'react-router-dom';
 import ReactDOM from 'react-dom';
+import 'semantic-ui-css/semantic.min.css'
 import './index.css';
 import App from './App';
 
