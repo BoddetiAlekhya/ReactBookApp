05d39f906f949dd4443b35fc857b2ecb65d43092
BoddetiAlekhya <boddeti.alekhya@gmail.com>
2019-02-25 23:02:17 +0530

    Login-Logout Actions

--- src/index.js
+++ src/index.js
@@ -13,10 +13,17 @@ import App from './App';
 
 import rootReducer from './rootReducer'
 import * as serviceWorker from './serviceWorker';
+import {userLoggedIn} from './actions/auth'
 
 const store = createStore(
     rootReducer,composeWithDevTools(applyMiddleware(thunk))
-)
+);
+
+if(localStorage.bookwormJWT){
+    const user = {token: localStorage.bookwormJWT};
+    store.dispatch(userLoggedIn(user));
+}
+
 ReactDOM.render(
 <BrowserRouter>
 <Provider store={store}>
